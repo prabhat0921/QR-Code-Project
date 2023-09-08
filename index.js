0855a99d05741cd43dd3f7688a3da9fc3e93cc76
prabhat0921 <91479933+prabhat0921@users.noreply.github.com>
2023-09-08 14:36:24 +0530

    commit

--- index.js
+++ index.js
@@ -16,7 +16,19 @@ inquirer
 ])
 
 .then((answer)=>{
-  const url =  answer.URL;
-  
-  fs.writeFile('URL.txt', {answer})
+  const url = answer.URL;
+  var qr_svg = qr.image(url);
+  qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+  fs.writeFile("URL.txt", url, (err)=>{
+    if(err) throw err;
+    console.log('The file has been saved!')
+  });
 })
+.catch((error) => {
+  if (error.isTtyError) {
+    // Prompt couldn't be rendered in the current environment
+  } else {
+    // Something else went wrong
+  }
+});
